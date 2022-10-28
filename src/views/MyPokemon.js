7828ae8da0e3dbca55f6fe4dd55b1a75307cb2bc
Ilker Ciliv <ilkerciliv@gmail.com>
2022-10-28 09:38:53 -0700

    moving on to myPokemon

--- src/views/MyPokemon.js
+++ src/views/MyPokemon.js
@@ -1,22 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 
 export default function MyPokemon() {
-  // modalState is the pokemon object containing details of the pokemon, eg. abilities, stats
-  const [modalState, setModalState] = useState({});
-  // selectedPokemon containes the URL of the pokemon pointing to pokemon
-  const [selectedPokemon, setSelectedPokemon] = useState({});
+  const [myPokemons, setMyPokemons] = useState(null);
 
-  const handleModalClick = (poke, pokemon) => {
-    console.log("click", poke.id, poke.name);
-    setModalState(poke);
-    setSelectedPokemon(pokemon);
-  };
+  useEffect(() => {
+    fetch("http://localhost:6001/myPokemon")
+      .then((response) => response.json())
+      .then((data) => setMyPokemons(data));
+  }, []);
+
+  console.log(myPokemons);
+
+  if (!myPokemons)
+    return (
+      <div>
+        <h2>{"Loading..."}</h2>
+      </div>
+    );
 
   return (
     <div>
       <Header />
-      {"My Pokemon"}
+      {myPokemons.map((pokemon, idx) => {
+        return <h1 key={idx}>{pokemon.url}</h1>;
+      })}
     </div>
   );
 }
