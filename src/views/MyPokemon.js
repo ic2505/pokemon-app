0187b84e782870111f2163a498d7a101d761e542
Ilker Ciliv <ilkerciliv@gmail.com>
2022-10-28 09:59:21 -0700

    myPokemon page displaying favorited pokemons

--- src/views/MyPokemon.js
+++ src/views/MyPokemon.js
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import PokemonContainer from "../components/pokedex/PokemonContainer";
+import PokeModal from "../components/pokedex/PokeModal";
 
 export default function MyPokemon() {
   const [myPokemons, setMyPokemons] = useState(null);
+  const [modalState, setModalState] = useState({});
+  // selectedPokemon containes the URL of the pokemon pointing to pokemon
+  const [selectedPokemon, setSelectedPokemon] = useState({});
+
+  const handleModalClick = (poke, pokemon) => {
+    // console.log("click", poke.id, poke.name);
+    setModalState(poke);
+    setSelectedPokemon(pokemon);
+  };
 
   useEffect(() => {
     fetch("http://localhost:6001/myPokemon")
@@ -20,10 +30,13 @@ export default function MyPokemon() {
       </div>
     );
 
+  // modalState is the pokemon object containing details of the pokemon, eg. abilities, stats
+
   return (
     <div>
       <Header />
-      <PokemonContainer pokemons={myPokemons} />
+      <PokeModal modalState={modalState} selectedPokemon={selectedPokemon} />
+      <PokemonContainer pokemons={myPokemons} onModalClick={handleModalClick} />
     </div>
   );
 }
