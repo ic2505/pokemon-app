68e70514e9d39ffd4b4468d4b4d57a4df05d5b47
Ilker Ciliv <ilkerciliv@gmail.com>
2022-10-28 10:22:01 -0700

    error page

--- src/views/Error.js
+++ src/views/Error.js
@@ -5,7 +5,11 @@ export default function Error() {
   return (
     <div>
       <Header />
-      <h1>Error</h1>
+      <div className="flex" style={{ height: "75vh" }}>
+        <div className="m-auto">
+          <h1 className="font-bold text-2xl">404: Error</h1>
+        </div>
+      </div>
     </div>
   );
 }
